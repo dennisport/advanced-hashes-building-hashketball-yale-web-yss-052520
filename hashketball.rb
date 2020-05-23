f63068bf1ce985f9e5f84b4bac8f4871258c285f
Dennis Portillo <dennis.portillo@yale.edu>
2020-05-23 23:32:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -63,7 +63,65 @@ def game_hash
       ]
     }, 
     away: {
-      
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
+      {
+        player_name: "Alan Anderson",
+        number: 0,
+        shoe: 16,
+        points: 22,
+        rebounds: 12,
+        assists: 12,
+        steals: 3,
+        blocks: 1,
+        slam dunks: 1
+      }, 
+      {
+        player_name: "Reggie Evans",
+        number: 30,
+        shoe: 14,
+        points: 12,
+        rebounds: 12,
+        assists: 12,
+        steals: 12,
+        blocks: 12,
+        slam dunks: 7
+      }, 
+       {
+        player_name: "Brook Lopez",
+        number: 11,
+        shoe: 17,
+        points: 17,
+        rebounds: 19,
+        assists: 10,
+        steals: 3,
+        blocks: 1,
+        slam dunks: 15
+      }, 
+       {
+        player_name: "Ben Gordon",
+        number: 8,
+        shoe: 15,
+        points: 33,
+        rebounds: 11,
+        assists: 6,
+        steals: 3,
+        blocks: 8,
+        slam dunks: 5
+      }, 
+       {
+        player_name: "Kemba Walker",
+        number: 33,
+        shoe: 15,
+        points: 6,
+        rebounds: 12,
+        assists: 12,
+        steals: 7,
+        blocks: 5,
+        slam dunks: 12
+      }
+      ]
       
       
       
